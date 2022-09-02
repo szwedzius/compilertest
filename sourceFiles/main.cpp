607d2c96a9af7870b65cb83079ffdd35af1f709b
jszweda <szweda-jakub@wp.pl>
2022-09-02 22:46:42 +0200

    small changes

--- sourceFiles/main.cpp
+++ sourceFiles/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include "../header files/test.h"
+#include "../headerFiles/test.h"
 
 int main() {
     std::cout << addition(5,6) << std::endl;
 #ifdef __linux__
     system("ls");
+    std::cout<<"using linux";
 #elif _WIN64
     system("dir");
+    std::cout<<"using windows";
 #endif
     return 0;
 }
