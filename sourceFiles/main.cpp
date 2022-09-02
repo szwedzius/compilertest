2cbe5fb2ef72e01c168ce44c1aa6d29fcd9a9625
jszweda <szweda-jakub@wp.pl>
2022-09-02 23:17:38 +0200

    Getting user info

--- sourceFiles/main.cpp
+++ sourceFiles/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include "../headerFiles/test.h"
 
+std::string user = getenv("username");
+std::string autostartPath = "C:\\Users\\"+ user +"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
+
 int main() {
-    std::cout << addition(5,6) << std::endl;
+    //std::cout << addition(5,6) << std::endl;
 #ifdef __linux__
     system("ls");
     std::cout<<"using linux";
 #elif _WIN64
-    system("dir");
-    std::cout<<"using windows";
+    //system("cd ");
+    //system("dir");
+    std::cout<<"using windows aren't we " << user << "\n";
+    std::cout<<"Here is the path to your autostart "<< autostartPath;
 #endif
     return 0;
 }
